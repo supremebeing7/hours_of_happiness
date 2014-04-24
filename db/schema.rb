23cc7c94bc5ada13733cb14baebbbd31fd2a8fcc
Tara Scherner de la Fuente & Natalie Blackburn <student@epicodus.com>
2014-04-24 14:59:22 -0700

    add gem and migration for geocoding API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424180352) do
+ActiveRecord::Schema.define(version: 20140424215131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140424180352) do
     t.text     "hh_description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: true do |t|
