2d62b13434c74e21a1d4a2f8f4510a2d03cfeb21
Mark Lehman & Spyder Monkee McHenry <student@epicodus.com>
2014-04-24 10:32:24 -0700

    add name and username to registration and edit user pages, and to application controller parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :username, :name
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :username, :email, :password, :password_confirmation, :remember_me) }
   end
 end
