511368a6e9e8cde40d79c3d382b564b8b44f62c1
Mark Lehman <markopolo@gmail.com>
2014-04-25 17:25:10 -0700

    add photo_url to users with imported google picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  # Removed username and password validation because it interferes with Google / omniauth login
-  # validates :username, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
+  # Removed password validation because it interferes with Google / omniauth login
   # validates :password, :password_confirmation, presence: true
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
@@ -22,10 +22,15 @@ class User < ActiveRecord::Base
         user = User.create(name: data["name"],
           provider:access_token.provider,
           email: data["email"],
+          photo_url: data["image"].sub('https:', ''),
           uid: access_token.uid ,
           password: Devise.friendly_token[0,20],
         )
-          binding.pry
+        sliced_email = data["email"].slice(0...(data["email"].index('@')))
+        username = (sliced_email + user.id.to_s).parameterize
+        user.update(username: username)
+        binding.pry
+        return user
       end
     end
   end
