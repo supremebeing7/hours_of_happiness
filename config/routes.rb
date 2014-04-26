03cd8b99d84318c3d21ff2cdf894aa107aec71ad
Tara Scherner de la Fuente <taratirzah@gmail.com>
2014-04-25 17:08:53 -0700

    add Capybara testing and Factory Girl implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 HoursOfHappiness::Application.routes.draw do
   devise_for :users
+  #get "login" => "sessions#new", :as => "login"
   root to: 'pages#home'
 
   match '/home', { :via => :get, :to => 'pages#home' }
   resources :places
   resources :happy_hours
   resources :ratings
+  resources :password_resets
 end
